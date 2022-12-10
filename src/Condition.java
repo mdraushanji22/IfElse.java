dafca71d2a41410231fe5e43634a94fcf8bffe0d
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2022-12-10 22:26:59 +0530

    Initial commit

---
+++ src/Condition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Condition {
+    public static void main(String[] args) {
+        int x;
+        Scanner r=new Scanner(System.in);
+        x=r.nextInt();
+        if(x%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+
+}
