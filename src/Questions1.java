a46df912e185c1fd26bebabde1d34a03758686c6
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2022-12-11 14:28:49 +0530

    Loops for java

---
+++ src/Questions1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+//print sum of first natural numbers
+public class Questions1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1; i<11; i++){
+            System.out.println(i*n);
+
+        }
+
+
+    }
+}
