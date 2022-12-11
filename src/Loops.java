7f99a66dea372890a5191be8e0128abc9882fefc
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2022-12-11 12:00:28 +0530

    if else condition for java

---
+++ src/Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int Counter=0; Counter<100;Counter=Counter+1){
+            System.out.println("Md Raushan Jilani");
+        }
+    }
+}
